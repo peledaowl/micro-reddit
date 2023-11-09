7c30d16f3aca825da3ee180acb5ec0de13e5c3d8
webdev <webdev@Xubuntu.myguest.virtualbox.org>
2023-11-10 00:18:46 +0200

    Add validations for username and email in User model

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  has_secure_password
+end
